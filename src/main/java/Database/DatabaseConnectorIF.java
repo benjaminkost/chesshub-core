5eef96c162c14eab2bf8ff8f61383ede504709d9
AzadCe <95860194+AzadCe@users.noreply.github.com>
2023-04-04 14:57:46 +0200

    DatabaseConnector Interface
    Einstellungen für die Verbindung der Datenbank

---
+++ src/main/java/Database/DatabaseConnectorIF.java
@@ -0,0 +1,11 @@
+package Database;
+
+public interface DatabaseConnectorIF {
+	
+	final String HOST = "localhost";
+	final String DATABASENAME = "chessanalytics";
+	final String DBUSER = "root";
+	final String DBPW = "root";
+	
+
+}
