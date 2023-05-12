b9ed1ace87cc8cee9380c272a5ee1058711821e0
Topa25 <filip.topic25@gmail.com>
2023-05-12 15:33:42 +0200

    - Ihr hatet Recht mit Eclipse hahahaha

--- src/main/java/DAO/Interface/GameDaoIF.java
+++ src/main/java/DAO/Interface/GameDaoIF.java
@@ -6,44 +6,43 @@ import BusinessObjects.Game;
 
 public interface GameDaoIF {
 
-	final String TABLENAME = "game";
+	String TABLENAME = "game";
 
 	// Collumnnames of Databasetable
-	final String COL_GAME_ID = "Game_ID";
-	final String COL_EVENT = "Event";
-	final String COL_ROUND = "Round";
-	final String COL_SITE = "Site";
-	final String COL_DATE = "Date";
-	final String COL_MOVES = "Moves";
-	final String COL_RESULT = "Result";
-	final String COL_BLACK = "Black";
-	final String COL_WHITE = "White";
-	final String COL_COMMENT = "Comment";
+	String COL_GAME_ID = "Game_ID";
+	String COL_EVENT = "Event";
+	String COL_ROUND = "Round";
+	String COL_SITE = "Site";
+	String COL_DATE = "Date";
+	String COL_MOVES = "Moves";
+	String COL_RESULT = "Result";
+	String COL_BLACK = "Black";
+	String COL_WHITE = "White";
+	String COL_COMMENT = "Comment";
 	
 	//Queries
-	final String Q_SELECTALLGAMES = "SELECT * FROM " +TABLENAME+";";
-	final String Q_SELECTBYGAMEID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_GAME_ID+" = ? ;";
-	final String Q_SELECTGAMEBYUSERID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_BLACK+" = ? OR "+COL_WHITE+"=? ORDER BY GAME_ID DESC;";
-	final String Q_SELECTGAMEBYTEAMID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_BLACK+" IN (SELECT user_ID FROM user_has_teams WHERE team_ID = ?) OR "+COL_WHITE+" IN (SELECT user_ID FROM user_has_teams WHERE team_ID = ?) ORDER BY GAME_ID DESC;";
-	final String Q_SELECTGAMESWITHOUTOPPONENT = "SELECT * FROM "+TABLENAME+ " WHERE ("+COL_BLACK+" = 0 AND "+COL_WHITE+"!=?) OR ("+COL_BLACK+" != ? AND "+COL_WHITE+"=0) ;";
-	final String Q_UPDATEGAME = "UPDATE "+TABLENAME+" SET "+COL_EVENT+"=?, "+COL_ROUND+"=?,"+COL_SITE+"=?,"+COL_DATE+"=?,"+COL_MOVES+"=?, "+COL_RESULT+"=?, "+COL_BLACK+"=?, "+COL_WHITE+"=?, "+COL_COMMENT+"=?  WHERE "+ COL_GAME_ID+"=?;";
-	final String Q_DELETEGAME = "DELETE FROM "+TABLENAME+" WHERE "+COL_GAME_ID+"=?";
-	final String Q_INSERTGAME = "INSERT INTO "+TABLENAME+"("+COL_EVENT+", "+COL_ROUND+", "+COL_SITE+", "+COL_DATE+", "+COL_MOVES+", "+COL_RESULT+", "+COL_BLACK+", "+COL_WHITE+", "+COL_COMMENT+") VALUES (?,?,?,?,?,?,?,?,?);";
+	String Q_SELECTALLGAMES = "SELECT * FROM " +TABLENAME+";";
+	String Q_SELECTBYGAMEID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_GAME_ID+" = ? ;";
+	String Q_SELECTGAMEBYUSERID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_BLACK+" = ? OR "+COL_WHITE+"=? ORDER BY GAME_ID DESC;";
+	String Q_SELECTGAMEBYTEAMID = "SELECT * FROM "+TABLENAME+ " WHERE "+COL_BLACK+" IN (SELECT user_ID FROM user_has_teams WHERE team_ID = ?) OR "+COL_WHITE+" IN (SELECT user_ID FROM user_has_teams WHERE team_ID = ?) ORDER BY GAME_ID DESC;";
+	String Q_SELECTGAMESWITHOUTOPPONENT = "SELECT * FROM "+TABLENAME+ " WHERE ("+COL_BLACK+" = 0 AND "+COL_WHITE+"!=?) OR ("+COL_BLACK+" != ? AND "+COL_WHITE+"=0) ;";
+	String Q_UPDATEGAME = "UPDATE "+TABLENAME+" SET "+COL_EVENT+"=?, "+COL_ROUND+"=?,"+COL_SITE+"=?,"+COL_DATE+"=?,"+COL_MOVES+"=?, "+COL_RESULT+"=?, "+COL_BLACK+"=?, "+COL_WHITE+"=?, "+COL_COMMENT+"=?  WHERE "+ COL_GAME_ID+"=?;";
+	String Q_DELETEGAME = "DELETE FROM "+TABLENAME+" WHERE "+COL_GAME_ID+"=?";
+	String Q_INSERTGAME = "INSERT INTO "+TABLENAME+"("+COL_EVENT+", "+COL_ROUND+", "+COL_SITE+", "+COL_DATE+", "+COL_MOVES+", "+COL_RESULT+", "+COL_BLACK+", "+COL_WHITE+", "+COL_COMMENT+") VALUES (?,?,?,?,?,?,?,?,?);";
 
-	public List<Game> getAllGames();
+	List<Game> getAllGames();
 
-	public Game getGameById(int game_id);
+	Game getGameById(int game_id);
 	
-	public List<Game> getGamesByUserId(int user_id);
+	List<Game> getGamesByUserId(int user_id);
 	
-	public List<Game> getGamesByTeamId(int team_id);
+	List<Game> getGamesByTeamId(int team_id);
 	
-	public List<Game> getGamesWithoutOpponent(int user_id);
+	List<Game> getGamesWithoutOpponent(int user_id);
 
-	public boolean updateGame(Game game);
+	boolean updateGame(Game game);
 
-	public boolean deleteGame(Game game);
+	boolean deleteGame(Game game);
 
-	public boolean insertGame(Game game);
-
+	boolean insertGame(Game game);
 }
