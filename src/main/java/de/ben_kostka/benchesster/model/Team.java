ae045173f575ebc94c596b95156c684a317cade1
Benjamin Kostka <mail@ben-kostka.de>
2024-08-01 14:13:35 +0200

    Initales SetUp für Migration von JakartaEE zu Spring Boot

---
+++ src/main/java/de/ben_kostka/benchesster/model/Team.java
@@ -0,0 +1,41 @@
+package de.ben_kostka.benchesster.model;
+
+public class Team {
+	private int team_ID;
+	private String name;
+	private Club club;
+	private User leader;
+	
+	public Team(String name, Club club, User leader) {
+		super();
+		this.name = name;
+		this.club = club;
+		this.leader = leader;
+	}
+
+	public Team() {
+		super();
+	}
+	public int getTeam_ID() {
+		return team_ID;
+	}
+	public void setTeam_ID(int team_ID) {
+		this.team_ID = team_ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Club getClub() {	return this.club;	}
+	public void setClub(Club club) {
+		this.club = club;
+	}
+	public User getLeader() {
+		return leader;
+	}
+	public void setLeader(User leader) {
+		this.leader = leader;
+	}
+}
