c165251f5cff2907464d0a27cb4c46a3fa2cef7f
Ben Kostka <104133262+benjaminkost@users.noreply.github.com>
2025-02-26 00:03:42 +0100

    Delete unnecessary Test BenChessterApplicationTests.java

--- web-app/src/test/java/de/ben_kostka/benchesster/BenChessterApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package de.ben_kostka.benchesster;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BenChessterApplicationTests {
-
-}
