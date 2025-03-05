4c1a7622a802ee8f5543baf7b038840a452a042d
Ben Kostka <mail@ben-kostka.de>
2025-03-05 15:22:42 +0100

    Tests erweitern mit Rollen zu User hinzufügen

--- web-app/src/test/java/de/ben_kostka/benchesster/repository/ClubRepositoryTests.java
+++ web-app/src/test/java/de/ben_kostka/benchesster/repository/ClubRepositoryTests.java
@@ -2,14 +2,17 @@ package de.ben_kostka.benchesster.repository;
 
 import de.ben_kostka.benchesster.AbstractTestcontainers;
 import de.ben_kostka.benchesster.model.Club;
+import de.ben_kostka.benchesster.model.Role;
 import de.ben_kostka.benchesster.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -39,6 +42,12 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         testUser.setPhone("1234");
         testUser.setTeams(null);
 
+        Role testRole = new Role();
+        testRole.setName("TEST_ROLE");
+        Set<Role> testRoles = new HashSet<>();
+        testRoles.add(testRole);
+        testUser.setRoles(testRoles);
+
         Club testClub = new Club();
         testClub.setName("TestClub");
         testClub.setPresident(testUser);
@@ -46,12 +55,11 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         //When
         underTest.save(testClub);
         Club savedClub = underTest.findById(1L).orElse(null);
-        Club savedClub = underTest.findAll().get(0);
 
         //Then
         Assertions.assertNotNull(savedClub);
         Assertions.assertEquals("TestClub", savedClub.getName());
-        Assertions.assertEquals(1, savedClub.getPresident().getUser_ID());
+        Assertions.assertEquals(1, savedClub.getPresident().getId());
     }
 
     @Test  //President von einem Verein wird verändert
@@ -64,12 +72,17 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         testUser.setEmail("test@mail.de");
         testUser.setPassword("test_password");
         testUser.setPhone("1234");
-        testUser.setUserStatus(1);
         testUser.setTeams(null);
 
         Club testClub = new Club();
         testClub.setName("TestClub");
         testClub.setPresident(testUser);
+
+        Role testRole = new Role();
+        testRole.setName("TEST_ROLE");
+        Set<Role> testRoles = new HashSet<>();
+        testRoles.add(testRole);
+        testUser.setRoles(testRoles);
 
         //When
         underTest.save(testClub);
@@ -78,8 +91,6 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         //Then
         Assertions.assertNotNull(savedClub);
         Assertions.assertEquals("TestClub", savedClub.getName());
-        Assertions.assertEquals(1, savedClub.getPresident().getId());
-        Assertions.assertEquals(1, savedClub.getPresident().getUser_ID());
 
         //When
         User testUser2 = new User();
@@ -89,7 +100,6 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         testUser2.setEmail("test@mail.de2");
         testUser2.setPassword("test_password2");
         testUser2.setPhone("12345");
-        testUser2.setUserStatus(1);
         testUser2.setTeams(null);
 
         testClub.setPresident(testUser2); //neuer Präsident wird festgelegt
@@ -103,7 +113,6 @@ public class ClubRepositoryTests extends AbstractTestcontainers {
         Assertions.assertEquals("test@mail.de2", savedClub.getPresident().getEmail());
         Assertions.assertEquals("test_password2", savedClub.getPresident().getPassword());
         Assertions.assertEquals("12345", savedClub.getPresident().getPhone());
-        Assertions.assertEquals(1, savedClub.getPresident().getUserStatus());
         Assertions.assertNull(savedClub.getPresident().getTeams());
     }
 
